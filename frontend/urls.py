aa45039f5e253ff4a08c10c4bf6af7b814020e91
alexsandro-martinz <martinz.alexsandro@gmail.com>
2023-07-09 17:50:29 -0300

    create links on base template

--- frontend/urls.py
+++ frontend/urls.py
@@ -6,7 +6,9 @@ from frontend import views
 app_name = 'frontend'
 
 urlpatterns = [
-    path('', views.loginView, name='login'),
+    path('', views.LoginView.as_view(), name='login'),
     path('logout', views.logoutView, name='logout'),
     path('home', views.homeView, name='home'),
+    path('products', views.productsView, name='products'),
+    path('accounts', views.accountsView, name='accounts'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -4,31 +4,46 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
+from django.views import View
 
-def loginView(request):
-    if request.user.is_authenticated:
-        return redirect(reverse('frontend:home'))
 
-    if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
+@login_required
+def productsView(request):
+    return render(request, "frontend/products.html")
 
-        user = authenticate(username=username, password=password)
+class LoginView(View):
+    def get(self, request):
+        if request.user.is_authenticated:
+            return redirect(reverse("frontend:home"))
+
+        return render(request, "frontend/login.html")
+
+    def post(self, request):
+        user = authenticate(
+            username=request.POST["username"], password=request.POST["password"]
+        )
 
         if user is not None:
             login(request, user)
-            return redirect(reverse('frontend:home'))
+            return redirect(reverse("frontend:home"))
         else:
-            messages.add_message(request, messages.ERROR, "Error when logging in the user.Try again.")
+            messages.add_message(
+                request, messages.ERROR, "Error when logging in the user.Try again."
+            )
             messages.get_messages(request).used = True
+            return render(request, "frontend/login.html")
 
-    return render(request, "frontend/login.html")
+
 
 @login_required
 def homeView(request):
-   return render(request, "frontend/index.html")
+    return render(request, "frontend/home.html")
 
-
+@login_required
 def logoutView(request):
     logout(request)
-    return redirect(reverse('frontend:login'))
+    return redirect(reverse("frontend:login"))
+
+@login_required
+def accountsView(request):
+    return render(request, 'frontend/accounts.html')
